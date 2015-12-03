50ddede0a8c98c1e342a112480c7a06a39bfcb36
Ricardo Caceres <ricarjose@hotmail.com>
2015-12-03 15:10:03 -0430

    total por concepto fix

--- app/Http/Controllers/ReporteController.php
+++ app/Http/Controllers/ReporteController.php
@@ -15,7 +15,6 @@ class ReporteController extends Controller {
 	public function getReporteMensual(Request $request){
         $mes=$request->get('mes', \Carbon\Carbon::now()->month);
         $anno=$request->get('anno',  \Carbon\Carbon::now()->year);
-
         $modulos=\App\Modulo::where('aeropuerto_id', session('aeropuerto')->id )->get();
         $montos=[];
         $montosTotales=[];
@@ -26,29 +25,19 @@ class ReporteController extends Controller {
             foreach($modulos as $modulo){
                 if(!isset($montos[$primerDiaMes->format('d/m/Y')][$modulo->nombre]))
                     $montos[$primerDiaMes->format('d/m/Y')][$modulo->nombre]=[];
-
-
                 $montos[$primerDiaMes->format('d/m/Y')][$modulo->nombre]["total"]=\DB::table('facturas')
                     ->join('facturadetalles','facturas.id' , '=', 'facturadetalles.factura_id')
                     ->join('conceptos','conceptos.id' , '=', 'facturadetalles.concepto_id')
                     ->where('conceptos.modulo_id', "=",$modulo->id)
                     ->where('facturas.fecha', $primerDiaMes)->sum('facturas.total');
                 foreach($modulo->conceptos as $concepto){
-                    $montos[$primerDiaMes->format('d/m/Y')][$modulo->nombre][$concepto->nompre]=\DB::table('facturas')
-                        ->join('facturadetalles','facturas.id' , '=', 'facturadetalles.factura_id')
+                    $montos[$primerDiaMes->format('d/m/Y')][$modulo->nombre][$concepto->nompre]=\DB::table('facturadetalles')
+                        ->join('facturas','facturas.id' , '=', 'facturadetalles.factura_id')
                         ->join('conceptos','conceptos.id' , '=', 'facturadetalles.concepto_id')
                         ->where('conceptos.modulo_id', "=",$modulo->id)
                         ->where('facturadetalles.concepto_id', $concepto->id)
-                        ->where('facturas.fecha', $primerDiaMes)->sum('facturas.total');
-
-
-
+                        ->where('facturas.fecha', $primerDiaMes)->pluck('facturadetalles.totalDes');
                 }
-
-
-
-
-
                 if(!isset($montosTotales[$modulo->nombre]))
                     $montosTotales[$modulo->nombre]=[];
                 if(!isset($montosTotales[$modulo->nombre]["total"]))
@@ -58,12 +47,10 @@ class ReporteController extends Controller {
                     if(!isset($montosTotales[$modulo->nombre][$concepto->nompre]))
                         $montosTotales[$modulo->nombre][$concepto->nompre]=0;
                     $montosTotales[$modulo->nombre][$concepto->nompre]+=($montos[$primerDiaMes->format('d/m/Y')][$modulo->nombre][$concepto->nompre]);
-
                 }
             }
         }
         return view('reportes.reporteDiario', compact('modulos', 'montos', 'montosTotales','mes','anno'));
-
     }
 
 }
