dc48fae1c7c559fc70e667a45921b9e59523c58c
Ricardo Caceres <ricarjose@hotmail.com>
2016-03-09 15:20:04 -0430

    FONTS + tasa controller fix

--- app/Http/Controllers/TasaController.php
+++ app/Http/Controllers/TasaController.php
@@ -17,13 +17,22 @@ class TasaController extends Controller {
         $fecha=$request->get('fecha');
         $taquilla=$request->get('taquilla');
         $turno=$request->get('turno');
-        $tasaOp=\App\Tasaop::firstOrCreate([
+        $tasaOp=\App\Tasaop::where([
+            'aeropuerto_id' => $aeropuertoId,
+            'fecha' => \Carbon\Carbon::createFromFormat('d/m/Y', $fecha)->format('Y-m-d'),
+            'taquilla' => $taquilla,
+            'turno' => $turno,
+        ])->first();
+        if(!$tasaOp){
+            $tasaOp= \App\Tasaop::create([
                 'aeropuerto_id' => $aeropuertoId,
                 'fecha' => \Carbon\Carbon::createFromFormat('d/m/Y', $fecha)->format('Y-m-d'),
                 'taquilla' => $taquilla,
                 'turno' => $turno,
             ]);
+        }
         $tasaOp->load('detalles');
+
         $series=["A", "B", "C", "D"];
         foreach($series as $value){
             $series[$value]=\DB::table('tasaopdetalles')->where('serie', $value)->max('fin')+1;
@@ -42,19 +51,19 @@ class TasaController extends Controller {
             'taquilla' => $taquilla,
             'turno' => $turno,
         ])->first();
-
+        $tasaOp->detalles()->delete();
         $detalles=$request->only('serie', 'desde', 'hasta', 'cantidad', 'monto');
-
-        foreach($detalles['serie'] as $serieIndex => $serie){
-            $tasaOp->detalles()->create([
-                "serie" => $serie,
-                "inicio" => $detalles['desde'][$serieIndex],
-                "fin" => $detalles['hasta'][$serieIndex],
-                "costo" => $detalles['monto'][$serieIndex],
-                "cantidad" => $detalles['cantidad'][$serieIndex],
-                "total" => $detalles['monto'][$serieIndex] * $detalles['cantidad'][$serieIndex],
-            ]);
-        }
+        if(isset($detalles['serie']))
+            foreach($detalles['serie'] as $serieIndex => $serie){
+                $tasaOp->detalles()->create([
+                    "serie" => $serie,
+                    "inicio" => $detalles['desde'][$serieIndex],
+                    "fin" => $detalles['hasta'][$serieIndex],
+                    "costo" => $detalles['monto'][$serieIndex],
+                    "cantidad" => $detalles['cantidad'][$serieIndex],
+                    "total" => $detalles['monto'][$serieIndex] * $detalles['cantidad'][$serieIndex],
+                ]);
+            }
 
     }
 }
