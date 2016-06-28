9974331c8f7024acff41a70ccd95e21ac47fbb33
Ana Urquiola <anitaurquiola28@gmail.com>
2016-06-27 23:38:01 -0430

    Cambios

--- database/migrations/2015_01_15_114412_create_role_usuario_table.php
+++ database/migrations/2015_01_15_114412_create_role_usuario_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateRoleUserTable extends Migration
+class CreateRoleUsuarioTable extends Migration
 {
     /**
      * Run the migrations.
